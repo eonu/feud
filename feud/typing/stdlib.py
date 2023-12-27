49ec9ae2dbc97f1433f4d569fbd91f3780cb3a8a
Edwin (Ed) Onuonga <ed@hazy.com>
2023-12-27 23:31:16 +0000

    fix: define `__all__` for `feud.typing` module (#134)

--- feud/typing/stdlib.py
+++ feud/typing/stdlib.py
@@ -13,9 +13,32 @@
 - ``uuid``
 """
 
-from collections import deque
-from datetime import date, datetime, time, timedelta
-from decimal import Decimal
-from enum import Enum, IntEnum, StrEnum
-from pathlib import Path
-from uuid import UUID
+from __future__ import annotations
+
+import collections
+import datetime
+import decimal
+import enum
+import pathlib
+import uuid
+from itertools import chain
+from types import ModuleType
+
+types: dict[ModuleType, list[str]] = {
+    collections: ["deque"],
+    datetime: ["date", "datetime", "time", "timedelta"],
+    decimal: ["Decimal"],
+    enum: ["Enum", "IntEnum", "StrEnum"],
+    pathlib: ["Path"],
+    uuid: ["UUID"],
+}
+
+globals().update(
+    {
+        attr: getattr(module, attr)
+        for module, attrs in types.items()
+        for attr in attrs
+    }
+)
+
+__all__ = list(chain.from_iterable(types.values()))
