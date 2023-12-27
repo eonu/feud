ef8ff2012cbb78cb48b8110e7f92431929160e69
Edwin Onuonga <edwinonuonga@gmail.com>
2023-12-27 22:51:39 +0000

    lower click and raise pydantic minimum versions + fix typing versioning

--- feud/typing/pydantic.py
+++ feud/typing/pydantic.py
@@ -5,66 +5,92 @@
 
 """Officially supported types from the ``pydantic`` package."""
 
-from pydantic import (
-    UUID1,
-    UUID3,
-    UUID4,
-    UUID5,
-    AmqpDsn,
-    AnyHttpUrl,
-    AnyUrl,
-    AwareDatetime,
-    Base64Bytes,
-    Base64Str,
-    ByteSize,
-    CockroachDsn,
-    DirectoryPath,
-    EmailStr,
-    FilePath,
-    FileUrl,
-    FiniteFloat,
-    FutureDate,
-    FutureDatetime,
-    HttpUrl,
-    ImportString,
-    IPvAnyAddress,
-    IPvAnyInterface,
-    IPvAnyNetwork,
-    Json,
-    KafkaDsn,
-    MariaDBDsn,
-    MongoDsn,
-    MySQLDsn,
-    NaiveDatetime,
-    NameEmail,
-    NegativeFloat,
-    NegativeInt,
-    NewPath,
-    NonNegativeFloat,
-    NonNegativeInt,
-    NonPositiveFloat,
-    NonPositiveInt,
-    PastDate,
-    PastDatetime,
-    PositiveFloat,
-    PositiveInt,
-    PostgresDsn,
-    RedisDsn,
-    SecretBytes,
-    SecretStr,
-    SkipValidation,
-    StrictBool,
-    StrictBytes,
-    StrictFloat,
-    StrictInt,
-    StrictStr,
-    conbytes,
-    condate,
-    condecimal,
-    confloat,
-    confrozenset,
-    conint,
-    conlist,
-    conset,
-    constr,
+from __future__ import annotations
+
+__all__ = []
+
+import packaging.version
+import pydantic
+
+version: packaging.version.Version = packaging.version.parse(
+    pydantic.__version__,
 )
+
+if version >= packaging.version.parse("2.0.3"):
+    __all__.extend(
+        [
+            "UUID1",
+            "UUID3",
+            "UUID4",
+            "UUID5",
+            "AmqpDsn",
+            "AnyHttpUrl",
+            "AnyUrl",
+            "AwareDatetime",
+            "Base64Bytes",
+            "Base64Str",
+            "ByteSize",
+            "CockroachDsn",
+            "DirectoryPath",
+            "EmailStr",
+            "FilePath",
+            "FileUrl",
+            "FiniteFloat",
+            "FutureDate",
+            "FutureDatetime",
+            "HttpUrl",
+            "ImportString",
+            "IPvAnyAddress",
+            "IPvAnyInterface",
+            "IPvAnyNetwork",
+            "Json",
+            "KafkaDsn",
+            "MariaDBDsn",
+            "MongoDsn",
+            "MySQLDsn",
+            "NaiveDatetime",
+            "NameEmail",
+            "NegativeFloat",
+            "NegativeInt",
+            "NewPath",
+            "NonNegativeFloat",
+            "NonNegativeInt",
+            "NonPositiveFloat",
+            "NonPositiveInt",
+            "PastDate",
+            "PastDatetime",
+            "PositiveFloat",
+            "PositiveInt",
+            "PostgresDsn",
+            "RedisDsn",
+            "SecretBytes",
+            "SecretStr",
+            "SkipValidation",
+            "StrictBool",
+            "StrictBytes",
+            "StrictFloat",
+            "StrictInt",
+            "StrictStr",
+            "conbytes",
+            "condate",
+            "condecimal",
+            "confloat",
+            "confrozenset",
+            "conint",
+            "conlist",
+            "conset",
+            "constr",
+        ]
+    )
+
+if version >= packaging.version.parse("2.4.0"):
+    types: list[str] = ["Base64UrlBytes", "Base64UrlStr"]
+
+    __all__.extend(types)
+
+if version >= packaging.version.parse("2.5.0"):
+    types: list[str] = ["JsonValue"]
+
+    __all__.extend(types)
+
+globals().update({attr: getattr(pydantic, attr) for attr in __all__})
