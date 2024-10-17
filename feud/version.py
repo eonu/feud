e467949b3bc59244dfbd1f3ddd9171df9e39543b
eonu <eonu@users.noreply.github.com>
2024-10-17 23:45:17 +0000

    release: v0.4.1

--- feud/version.py
+++ feud/version.py
@@ -33,7 +33,7 @@ Source code modified from pydantic (https://github.com/pydantic/pydantic).
 
 __all__ = ["VERSION", "version_info"]
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
 
 def version_info() -> str:
